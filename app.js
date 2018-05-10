ebff3a1cc46ed1e46989bea23ed704f451837251
Adadrsh <ou.adarsh@gmail.com>
2018-05-10 23:14:30 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+var nodemailer = require('nodemailer');
+var express = require('express');
+var app = express();
+
+
+app.get('/:email', function(req, res ) {
+    
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: 'ENTER YOUR EMAIL HERE',
+          pass: 'ENTER YOUR PASSWORD HERE '
+        }
+      });
+      
+
+
+    var mailOptions = {
+        from: 'ENTER EMAIL',
+        to: req.params.email,
+        subject: 'Sending Email using Node.js',
+        text: 'That was easy!'
+      };
+
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
+      
+
+
+});
+app.listen(5000);
+console.log('Server running at port 5000 ');
+
+
